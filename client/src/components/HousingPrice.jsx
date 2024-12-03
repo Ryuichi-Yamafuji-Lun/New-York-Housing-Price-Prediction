af1cef245ec212d976e1c7907a887ca972901044
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2024-12-02 16:56:00 -1000

    initial pages for the website

--- client/src/components/HousingPrice.jsx
+++ client/src/components/HousingPrice.jsx
@@ -1,1 +1,12 @@
 // Form for users to get a predicted price
+import React from 'react';
+
+function FormPage() {
+  return (
+    <div>
+        <h1>Form</h1>
+    </div>
+  );
+}
+
+export default FormPage;
--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,1 +1,12 @@
 // Welcome Page for the user
+import React from 'react';
+
+function WelcomePage() {
+  return (
+    <div>
+        <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default WelcomePage;
