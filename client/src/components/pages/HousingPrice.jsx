0aef051b423bc8c1ed84dc8a9abcb3038b5cfa0d
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2024-12-05 18:00:00 -1000

    create initial setup for the prediction page

--- client/src/components/pages/HousingPrice.jsx
+++ client/src/components/pages/HousingPrice.jsx
@@ -1,7 +1,24 @@
 // Form for users to get a predicted price
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function FormPage() {
+const FormPage = () => {
+  const [locations, setLocations] = useState([]);
+  const [formData, setFormData] = useState({
+    total_sqft: "",
+    location: "",
+    bhk: "",
+    baths: "",
+  });
+
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+  // fetch locations
+
+  // handle form change
+
+  // handle form submission
+  
   return (
     <div>
         <h1>Form</h1>
